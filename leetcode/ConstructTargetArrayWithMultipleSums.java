b50fbe5aeba3d02bf6bcd5bff36b152ea54d7e2e
wildana_dev <wildana.dev@gmail.com>
2026-05-19 10:56:22 +0700

    fix: integer overflow in problemm ConstructTargetArrayWithMultipleSums

--- leetcode/ConstructTargetArrayWithMultipleSums.java
+++ leetcode/ConstructTargetArrayWithMultipleSums.java
@@ -3,16 +3,16 @@ import java.util.PriorityQueue;
 public class ConstructTargetArrayWithMultipleSums {
   public boolean isPossible(int[] target) {
     if (target.length == 1) return target[0] == 1;
-    PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
-    int sum = 0;
-    for (int t : target) {
+    PriorityQueue<Long> pq = new PriorityQueue<>((a, b) -> Long.compare(b, a));
+    long sum = 0;
+    for (long t : target) {
       pq.add(t);
       sum += t;
     }
     while (pq.peek() != 1) {
-      int curr = pq.poll();
+      long curr = pq.poll();
       if (sum - curr == 1) return true;
-      int x = curr % (sum - curr);
+      long x = curr % (sum - curr);
       sum = sum - curr + x;
       if (x == 0 || x == curr) return false;
       else pq.add(x);
