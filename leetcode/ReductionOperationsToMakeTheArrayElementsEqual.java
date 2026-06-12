d6d773de37bf7c07444541cf498476dc7d03240d
wildana_dev <wildana.dev@gmail.com>
2026-06-12 20:39:13 +0700

    refactor: Leetcode problem Reduction Operations to Make Array Elements Equal

--- leetcode/ReductionOperationsToMakeTheArrayElementsEqual.java
+++ leetcode/ReductionOperationsToMakeTheArrayElementsEqual.java
@@ -1,20 +1,13 @@
 import java.util.Arrays;
-import java.util.HashMap;
 
 public class ReductionOperationsToMakeTheArrayElementsEqual {
   public int reductionOperations(int[] nums) {
-    int ans = 0, cntAns = 0;
-    if (nums.length == 1) return 0;
     Arrays.sort(nums);
-    HashMap<Integer, Integer> map = new HashMap<>();
-    for (int i : nums) map.put(i, map.getOrDefault(i, 0) + 1);
-    System.out.println(map);
-    for (int i : map.keySet()) {
-      if (i != nums[0]) {
-        System.out.println(i);
-        cntAns += map.get(i);
-        ans += cntAns;
-      }
+    int ans = 0;
+    int up = 0;
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] != nums[i - 1]) up++;
+      ans += up;
     }
     return ans;
   }
